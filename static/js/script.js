3eb1f818bc93f451f939ee54e1e371e480cca49c
Junmiann <Mimmixu@gmail.com>
2023-04-22 10:26:38 +0200

    Adjustments

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,36 @@
 //Dialogen
 const pfp_dialog = document.getElementById('pfp_dialog');
 const desc_dialog = document.getElementById('desc_dialog');
-const updatefavbook_dialog = document.getElementById('updatefavbook_dialog');
+const update_fav_book_dialog = document.getElementById('update_fav_book_dialog');
 const update_least_fav_book_dialog = document.getElementById('update_least_fav_book_dialog');
 const quote_dialog = document.getElementById('quote_dialog');
 
 //Knappen för dialogen
-const openpfpDialogBtn = document.getElementById('openpfpDialogBtn');
-const opendescDialogBtn = document.getElementById('opendescDialogBtn');
-const open_updatefavbook_DialogBtn = document.getElementById('open_updatefavbook_DialogBtn');
-const open_updateleastfavbook_DialogBtn = document.getElementById('open_updateleastfavbook_DialogBtn');
-const open_quote_DialogBtn = document.getElementById('open_quote_DialogBtn');
+const open_pfp_dialog_btn = document.getElementById('open_pfp_dialog_btn');
+const open_desc_dialog_btn = document.getElementById('open_desc_dialog_btn');
+const open_update_fav_book_dialog_btn = document.getElementById('open_update_fav_book_dialog_btn');
+const open_update_least_fav_book_dialog_btn = document.getElementById('open_update_least_fav_book_dialog_btn');
+const open_quote_dialog_btn = document.getElementById('open_quote_dialog_btn');
 
 
 //Om man klickar på "knappen" så öppnas respektive dialog
-openpfpDialogBtn.addEventListener('click', () => {
+open_pfp_dialog_btn.addEventListener('click', () => {
   pfp_dialog.showModal();
 });
-  
-opendescDialogBtn.addEventListener('click', () => {
+
+open_desc_dialog_btn.addEventListener('click', () => {
   desc_dialog.showModal();
 });
 
-open_updatefavbook_DialogBtn.addEventListener('click', () => {
-  updatefavbook_dialog.showModal();
+open_update_fav_book_dialog_btn.addEventListener('click', () => {
+  update_fav_book_dialog.showModal();
 });
 
-open_updateleastfavbook_DialogBtn.addEventListener('click', () => {
+open_update_least_fav_book_dialog_btn.addEventListener('click', () => {
   update_least_fav_book_dialog.showModal();
 });
 
-open_quote_DialogBtn.addEventListener('click', () => {
+open_quote_dialog_btn.addEventListener('click', () => {
   quote_dialog.showModal();
 });
 
@@ -48,9 +48,9 @@ desc_dialog.addEventListener('click', (e) => {
   }
 });
 
-updatefavbook_dialog.addEventListener('click', (e) => {
-  if (e.target === updatefavbook_dialog) {
-    updatefavbook_dialog.close();
+update_fav_book_dialog.addEventListener('click', (e) => {
+  if (e.target === update_fav_book_dialog) {
+    update_fav_book_dialog.close();
   }
 });
 
